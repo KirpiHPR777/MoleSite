f13492d183df7966bc2b5bde04c8b1e13e0a70e7
HeronukoPoRoyalu777 <sophiakirpichenok@gmail.com>
2022-07-17 16:17:48 +0300

    fix db url

--- server/database.js
+++ server/database.js
@@ -1,12 +1,2 @@
 const {Sequelize} = require('sequelize');
-
-module.exports = new Sequelize (
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host:process.env.DB_HOST || process.env.DATABASE_URL,
-        port: process.env.DB_PORT
-    }
-);
+module.exports = new Sequelize(process.env.DATABASE_URL);

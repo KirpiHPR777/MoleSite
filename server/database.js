a1a96064f05e4a9554f56d92b0f22e5d64e67e68
HeronukoPoRoyalu777 <sophiakirpichenok@gmail.com>
2022-07-17 16:29:18 +0300

    fix connection to db try 5

--- server/database.js
+++ server/database.js
@@ -1,7 +1,2 @@
 const {Sequelize} = require('sequelize');
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: true
-    }
-});
+module.exports = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});

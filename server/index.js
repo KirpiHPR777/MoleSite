7e54f9311382ed8b66d98df463abd0b2858c9084
HeronukoPoRoyalu777 <sophiakirpichenok@gmail.com>
2022-07-18 07:01:04 +0300

    fix cors options try 1

--- server/index.js
+++ server/index.js
@@ -8,14 +8,15 @@ const router = require('./routes/mainRouter');
 const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
+const corsOptions ={ origin: '*', credentials: true, optionSuccessStatus:200,}
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
-
+ 
 const start = async () => {
     try{
         await sequelize.authenticate();

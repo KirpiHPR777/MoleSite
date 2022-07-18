4af0697dde3f4ce9088dfed36544abfa3b62a4b6
HeronukoPoRoyalu777 <sophiakirpichenok@gmail.com>
2022-07-18 09:39:00 +0300

    fix cors try 4

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const errorHandler = require('./middleware/errorHandlingMiddleware');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(cors({original: ['https://molesite.netlify.app/']}));
 app.use(express.json());
 app.use(fileUpload({}));
 app.use('/api', router);
